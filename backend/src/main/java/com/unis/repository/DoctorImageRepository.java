9a1724e494812ff0dcc653cd9d9e5ffac221ccfd
humbertovenavente <jnajar@unis.edu.gt>
2025-02-27 22:02:18 -0600

    reversing changes since there was an error on doctor table

--- backend/src/main/java/com/unis/repository/DoctorImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.unis.repository;
-
-import com.unis.model.DoctorImage;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class DoctorImageRepository implements PanacheRepository<DoctorImage> {
-}
