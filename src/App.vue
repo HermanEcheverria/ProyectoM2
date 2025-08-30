7112a7a431822afb8f04d4d4d993238d0f9c8577
HermanEcheverria <hecheverria@unis.edu.gt>
2025-08-30 14:26:17 -0600

    primer test

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ const myAccountRoute = () => {
 </script>
 
 <template>
-  <header class="navbar">
+  <header class="navbarr">
     <div class="navbar-container">
       <div class="brand">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
