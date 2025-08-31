779a434c7c9214c1883b99855bbda8d70bc02b85
HermanEcheverria <hecheverria@unis.edu.gt>
2025-08-30 21:04:51 -0600

    Test de pipeline

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ const myAccountRoute = () => {
       <div class="brand">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
 
-        <h1>Dev </h1>
+        <h1>Dev TEST </h1>
       </div>
 
       <nav class="nav-links">
