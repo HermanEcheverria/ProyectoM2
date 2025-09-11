0eb3ae754a150199579dd97ce497bd680827daaf
HermanEcheverria <hecheverria@unis.edu.gt>
2025-09-11 16:55:18 -0600

    FIX

--- backend/src/main/java/com/unis/service/ReporteModeracionExcelService.java
+++ backend/src/main/java/com/unis/service/ReporteModeracionExcelService.java
@@ -62,7 +62,7 @@ public class ReporteModeracionExcelService {
         header.createCell(0).setCellValue("#");
         header.createCell(1).setCellValue("Usuario");
         header.createCell(2).setCellValue("Total Rechazos");
-        if (true) 
+     
 
         for (ModeracionReporteDTO dto : datos) {
             Row fila = sheet.createRow(rowNum++);
